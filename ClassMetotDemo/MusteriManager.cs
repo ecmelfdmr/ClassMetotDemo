0782b44d339085b5359718f68c9e476e368fb13a
Ecem Demir <ecmelfdmr2@hotmail.com>
2021-02-16 21:35:47 +0300

    3.Gün Ödevi

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Sisteme Müşteri Eklendi.");
+            Console.WriteLine(musteri.Id + " " + musteri.MusterininAdi + " " + musteri.MusterininSoyadi + " " + musteri.MusterininYasi + " ");
+        }
+
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine("Sistemden Müşteri Silindi.");
+            Console.WriteLine(musteri.Id + " " + musteri.MusterininAdi + " " + musteri.MusterininSoyadi + " " + musteri.MusterininYasi + " ");
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 3698;
+            musteri1.MusterininAdi = "Selin";
+            musteri1.MusterininSoyadi = "Kaya";
+            musteri1.MusterininYasi = 20;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 4568;
+            musteri2.MusterininAdi = "Ada";
+            musteri2.MusterininSoyadi = "Gül";
+            musteri2.MusterininYasi = 27;
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 1235;
+            musteri3.MusterininAdi = "Efe";
+            musteri3.MusterininSoyadi = "Demir";
+            musteri3.MusterininYasi = 18;
+
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+            foreach (var musteri in musteriler)
+            {
+                Console.WriteLine("Id Numarası : " + musteri.Id);
+                Console.WriteLine("Ad : " + musteri.MusterininAdi);
+                Console.WriteLine("Soyad : " + musteri.MusterininSoyadi);
+                Console.WriteLine("Yaş : " + musteri.MusterininYasi);
+                Console.WriteLine("------------");
+
+            }
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri3);
+
+            MusteriManager musteriManager1 = new MusteriManager();
+            musteriManager1.Sil(musteri2);
+        }
+    }
+}
